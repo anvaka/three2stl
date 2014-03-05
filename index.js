b859142daf11a5d8b0e349d33097ad28b7bc952f
anvaka <anvaka@gmail.com>
2014-03-05 01:53:43 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Exports three.js scene into stl format (stereo lithography)
+ * This file is based on work of viktor kovács (http://kovacsv.hu/)
+ *
+ * @author Andrei Kashcha
+ */
+module.exports = {
+  scene: exportScene,
+  meshes: exportMeshes
+};
+
+function exportScene(scene) {
+  var meshes = [];
+  scene.traverse (function (obj) {
+    if (obj instanceof THREE.Mesh) meshes.push (obj);
+  });
+
+  return exportMeshes();
+}
+
+function exportMeshes(meshes) {
+  var result = [];
+  add('solid exported');
+
+  var i, j, mesh, geometry, face, matrix, position;
+  var normal, vertex1, vertex2, vertex3;
+
+  for (i = 0; i < meshes.length; i++) {
+    mesh = meshes[i];
+
+    geometry = mesh.geometry;
+    matrix = mesh.matrix;
+    position = mesh.position;
+
+    for (j = 0; j < geometry.faces.length; j++) {
+      face = geometry.faces[j];
+      normal = face.normal;
+      vertex1 = getTransformedPosition(geometry.vertices[face.a], matrix, position);
+      vertex2 = getTransformedPosition(geometry.vertices[face.b], matrix, position);
+      vertex3 = getTransformedPosition(geometry.vertices[face.c], matrix, position);
+      addTriangle(normal, vertex1, vertex2, vertex3);
+    }
+  }
+
+  add('endsolid exported');
+
+  return result.join('\n');
+
+  function add(line) {
+    result.push(line);
+  }
+
+  function addTriangle(normal, vertex1, vertex2, vertex3) {
+    add('\tfacet normal ' + normal.x + ' ' + normal.y + ' ' + normal.z);
+    add('\t\touter loop');
+    add('\t\t\tvertex ' + vertex1.x + ' ' + vertex1.y + ' ' + vertex1.z);
+    add('\t\t\tvertex ' + vertex2.x + ' ' + vertex2.y + ' ' + vertex2.z);
+    add('\t\t\tvertex ' + vertex3.x + ' ' + vertex3.y + ' ' + vertex3.z);
+    add('\t\tendloop');
+    add('\tendfacet');
+  }
+
+  function getTransformedPosition(vertex, matrix, position) {
+    var result = vertex.clone();
+    if (matrix !== undefined) {
+      result.applyMatrix4(matrix);
+    }
+    if (position !== undefined) {
+      result.add(position);
+    }
+
+    return result;
+  }
+}
